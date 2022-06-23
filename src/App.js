8c00f48e342ffc0522782cf354f7f6473dc83842
docmartino <mrrolandmartin@gmail.com>
2022-06-23 16:54:43 +0200

    auth0 test

--- src/App.js
+++ src/App.js
@@ -1,89 +1,35 @@
 import "./styles/App.css";
-import React from "react";
-import { useState } from "react";
-import axios from "./api/axios";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
-  const [email, setEmail] = useState("");
-  const [pwd, setPwd] = useState("");
-  const [Auth, setAuth] = useState(false);
-  const [success, setSuccess] = useState(false);
-  cosnt [errMsg, setErrMsg] = useState("");
-
-  const Submit = (e) => {
-    e.preventDefault();
-    console.log(email + " " + pwd);
-
-    const user = { email: email, pwd: pwd };
-
-
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    // console.log(email, pwd);
-    try {
-      const userSend = {
-        password: `${pwd}`,
-        email: `${email}`,
-        username: `${email}`,
-      };
-      // userPast = userSend;
-      const response = await axios.post("/users", JSON.stringify(userSend), {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        // withCredentials: true
-      });
-      console.log(JSON.stringify(response.data));
-      const accessToken = response.data.token;
-
-      setAuth({ email, pwd, accessToken });
-      setEmail("");
-      setPwd("");
-      setSuccess(true);
-    } catch (err) {
-      if (!err.response) {
-        setErrMsg("No server Response");
-      } else if (err.response.status === 400) {
-        setErrMsg("Incorrect email or password");
-      } else if (err.response.status === 401) {
-        setErrMsg("Unauthorized");
-      } else {
-        setErrMsg("Login Failed");
-      }
-      errRef.current.focus();
-    }
-  }
+  const { isLoading } = useAuth0();
+  if (isLoading) return <div>Loading...</div>;
   
-  // const FetchUsers = async () => {
-  //   try {
-  //     const response = await axios.post("/users", JSON.stringify(user), {
-  //       headers: { "Content-Type": "application/json" },
-  //     });
-  //     console.log(response.data);
-  //   } catch (err) {
-  //     if (err.response) {
-  //       console.log(err.response);
-  //     } else {
-  //       console.log(`Error: ${err.message}`);
-  //     }
-  //   }
-  //   setPwd("");
-  //   setEmail("");
-  // };
-
-
   return (
     <div className="App">
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
+
+/*
+
       <div className="logContainer">
         <div className="log">
           <form className="form">
             <label>
               <input
-                type="email"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                type="text"
+                placeholder="UserName"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
               />
             </label>
             <label>
@@ -94,14 +40,12 @@ function App() {
                 onChange={(e) => setPwd(e.target.value)}
               />
             </label>
-            <button type="submit" onClick={Submit}>
+            <button type="submit" onClick={HandleSubmit}>
               LOGIN
             </button>
           </form>
         </div>
       </div>
-    </div>
-  );
-}
 
-export default App;
+
+      */
