e0ca564dd763fce5643bf29f89b2eb15612fbd7f
docmartino <mrrolandmartin@gmail.com>
2022-06-27 12:31:40 +0200

    test

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
 import Profile from "./components/Profile";
 import { useAuth0 } from "@auth0/auth0-react";
+import NavBar from "./structure/navBar";
 
 function App() {
   const { isLoading } = useAuth0();
   if (isLoading) return <div>Loading...</div>;
-  
+
   return (
-    <div className="App">
-      <LoginButton />
-      <LogoutButton />
-      <Profile />
-    </div>
+    <>
+      <NavBar />
+      <div className="App">
+        <LoginButton />
+        <LogoutButton />
+        <Profile />
+      </div>
+    </>
   );
 }
 
---
+++ src/structure/navBar.js
@@ -0,0 +1,5 @@
+const NavBar = () => {
+  return <nav className="navbar navbar-expand-lg navbar-light bg-light" />;
+};
+
+export default NavBar;
